7c919ff82e5f074d670316645044f3b710698893
MIA HAGOOD (771657) <771657@lcps.org>
2019-03-31 21:49:25 -0400

    Autofill email attempt phase 2

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@
             }
         });
     };
-    // Call the functions 
+    // Call the functions
     magnifPopup();
 
     //Background Parallax
@@ -116,7 +116,7 @@
 
 
 
-    //Function to animate slider captions 
+    //Function to animate slider captions
     function doAnimations(elems) {
         //Cache the animationend event in a variable
         var animEndEv = 'webkitAnimationEnd animationend';
@@ -130,21 +130,21 @@
         });
     }
 
-    //Variables on page load 
+    //Variables on page load
     var $myCarousel = $('.caption-slider'),
         $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
 
-    //Initialize carousel 
+    //Initialize carousel
     $myCarousel.carousel();
 
-    //Animate captions in first slide on page load 
+    //Animate captions in first slide on page load
     doAnimations($firstAnimatingElems);
 
-    //Pause carousel  
+    //Pause carousel
     $myCarousel.carousel('pause');
 
 
-    //Other slides to be animated on carousel slide event 
+    //Other slides to be animated on carousel slide event
     $myCarousel.on('slide.bs.carousel', function (e) {
         var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
         doAnimations($animatingElems);
@@ -190,7 +190,9 @@
             }
         });
 
-
+    function sendMail() {
+      window.open('mailto:doogahiam@gmail.com?subject=subject&body=body');
+    }
 
 
 
